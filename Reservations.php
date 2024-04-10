0f5055dd0ecdab6d43a6397dde8f9021a619c673
grantjones11 <grant.jones@bobcats.gcsu.edu>
2024-04-10 12:02:42 -0700

    Added reservation buttons

--- Reservations.php
+++ Reservations.php
@@ -39,6 +39,27 @@ session_start();
 
         <h2>Reservations</h2>
         
+        <form method ="post">
+            <?php
+
+            $start_time = strtotime('7:00 AM');
+            $end_time = strtotime('5:00 PM');
+            $interval = 30 * 60;
+
+            for ($time = $start_time; $time < $end_time; $time += $interval){
+                echo '<button type="submit" name="time" value="' . date('H:i', $time) . '">' . date('h:i A', $time) . '</button>';
+
+            }
+        ?>
+        <br>
+        <label for = "username">Your Username:</label>
+        <input type = "text" name = "username" id="username" required>
+        <br>
+        <label for ="phone">Your Phone Number: </label>
+        <input type = "text" name = "phone" id = "phone" required>
+        <br>
+        </form>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
                 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
                 crossorigin="anonymous">
