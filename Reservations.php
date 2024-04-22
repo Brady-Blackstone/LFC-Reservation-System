ae057a32be43729a49894d66368ebe6d736e1cb9
grantjones11 <grant.jones@bobcats.gcsu.edu>
2024-04-22 12:56:25 -0400

    Reservations page updated w info

--- Reservations.php
+++ Reservations.php
@@ -73,13 +73,13 @@ if (!isset($_SESSION['user']) && !isset($_SESSION['admin']))
                         </tr>
                         </thead>
                         <tbody>
-                            
+
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-
+        <script src = "./js/reservation.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
                 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
                 crossorigin="anonymous">
